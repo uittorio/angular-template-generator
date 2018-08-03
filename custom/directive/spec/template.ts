77853accd4fe1762443991a4114e86fa583c6f49
Edward Januszewski <e.januszewski@gmail.com>
2018-08-03 11:28:05 +0100

    fix spacing

--- custom/directive/spec/template.ts
+++ custom/directive/spec/template.ts
@@ -3,22 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { $Directive } from '$DirectiveFile';
 
 describe('$Directive', () => {
-	let fixture: ComponentFixture<TestHostComponent>;
-	
-	@Component({
-		template: `<div $Selector></div>`
-	})
-	class TestHostComponent {
-	}
-	
-	beforeEach(async(async() => {
-		await TestBed.configureTestingModule({
-			declarations: [
-				TestHostComponent,
-				$Directive
-			]
-		}).compileComponents();
+  let fixture: ComponentFixture<TestHostComponent>;
 
-		fixture = TestBed.createComponent(TestHostComponent);
-	}));
+  @Component({
+    template: `<div $Selector></div>`
+  })
+  class TestHostComponent {
+  }
+
+  beforeEach(async(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        TestHostComponent,
+        $Directive
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+  }));
 });
